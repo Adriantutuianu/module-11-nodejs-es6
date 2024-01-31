cf73ddd30251968a4b6b050f62b992fff02359eb
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-31 18:55:18 +0000

    Parameter check task

---
+++ 02-node-and-npm-lesson/activities/02-Stu-Parameter-Check/index.js
@@ -0,0 +1,9 @@
+// console.log(process.argv[2]);
+const var1 = process.argv[2];
+
+const var2 = process.argv[3];
+console.log(var1, var2);
+
+console.log(var1 === var2);
+
+console.log(var1 === var2 ? true : false);
