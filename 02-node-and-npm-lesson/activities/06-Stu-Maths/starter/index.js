bd7a3405fe59a87dec3b904f3a99c332e5214b64
Adrian Tut <adriantutuianu01@gmail.com>
2024-01-31 20:26:33 +0000

    Npm example usage

--- 02-node-and-npm-lesson/activities/06-Stu-Maths/starter/index.js
+++ 02-node-and-npm-lesson/activities/06-Stu-Maths/starter/index.js
@@ -3,21 +3,21 @@ const maths = require("./maths.js");
 // TODO: Capture the values passed from the command line into these three variables: `operation`, `numOne` and `numTwo`
 const operation = process.argv[2];
 
-const var1 = parseInt(process.argv[3]);
-const var2 = parseInt(process.argv[4]);
+const numOne = parseInt(process.argv[3]);
+const numTwo = parseInt(process.argv[4]);
 // TODO: Create a `switch` statement that accepts an `operation` parameter
 switch (operation) {
   case "sum":
-    console.log(maths.sum(var1, var2));
+    console.log(maths.sum(numOne, numTwo));
     break;
   case "difference":
-    console.log(maths.difference(var1, var2));
+    console.log(maths.difference(numOne, numTwo));
     break;
   case "product":
-    console.log(maths.product(var1, var2));
+    console.log(maths.product(numOne, numTwo));
     break;
   case "quotient":
-    console.log(maths.quotient(var1, var2));
+    console.log(maths.quotient(numOne, numTwo));
     break;
   default:
     console.log("Check your math!");
